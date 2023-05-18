6b020ee936e5daf75b175e84278583454847b38d
gastonmatias <g.matiasvillagra@gmail.com>
2023-05-18 17:54:07 -0400

    Added - convivencia entre API Mock y API Real

--- services/login.ts
+++ services/login.ts
@@ -1,7 +1,8 @@
+import { baseURL } from "@/config"
 import axios from "axios"
 
 export const loginService = async (email: string, password: string): Promise<void> => {
-    return axios.post('/login',{
+    return axios.post(`${baseURL}/login`,{
         email,
         password
     })
