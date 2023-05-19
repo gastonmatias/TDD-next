3a611e78890846e9c314ca4d74bd1faf7a083ed6
gastonmatias <g.matiasvillagra@gmail.com>
2023-05-19 11:49:51 -0400

    RED - should render a create product form page

--- __tests__/pages/products/create.test.tsx
+++ __tests__/pages/products/create.test.tsx
@@ -1,6 +1,14 @@
+import CreateProductPage from "@/pages/products/create"
+import { render, screen } from "@testing-library/react"
 
 //!  tests for user history "Store Form App"
 describe('when the form is mounted', () => {
     
+    //! 1) Acceptance Criteria (AC): There must be a create product form page.----
+    it('should render a create product form page', () => {
+      render(<CreateProductPage/>)  
+
+      expect(screen.queryByText(/create product/i)).toBeInTheDocument()
+    })
 
 })
