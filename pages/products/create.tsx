e749812d855e7d4231ee0ecedd9c02fa3061c23c
gastonmatias <g.matiasvillagra@gmail.com>
2023-05-19 12:11:18 -0400

    GREEN - should render a create product form page

--- pages/products/create.tsx
+++ pages/products/create.tsx
@@ -2,7 +2,9 @@ import { NextPage } from "next"
 
 const CreateProductPage: NextPage = () => {
   return (
-    <></>
+    <>
+    <h1>Create Product</h1>
+    </>
   )
 }
 export default CreateProductPage
