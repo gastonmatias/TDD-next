5241a99b48f5c4a76e35ef288c28eff240fb5016
gastonmatias <g.matiasvillagra@gmail.com>
2023-05-19 14:50:38 -0400

    GREEN - it must have the fields name, size and select type

--- pages/products/create.tsx
+++ pages/products/create.tsx
@@ -1,9 +1,49 @@
+import { Typography, TextField, MenuItem, FormControl, InputLabel, Select, Button } from "@mui/material"
 import { NextPage } from "next"
 
 const CreateProductPage: NextPage = () => {
   return (
     <>
-    <h1>Create Product</h1>
+    <Typography variant="h1" color="initial">Create Product</Typography>
+    
+    <form>
+        <TextField
+          id="name"
+          label="name"
+        />
+
+        <TextField
+          id="size"
+          label="size"
+        />
+
+        <FormControl fullWidth>
+            <InputLabel htmlFor="type" id="type">Type</InputLabel>
+
+            <Select
+              native
+              aria-labelledby="type"
+              labelId="type"
+              role="type-select"
+              id="type"
+            //   value=''
+              label="Type"
+              inputProps={{
+                name: 'type',
+                id:'type',
+                "data-testid": 'type-select'
+              }}
+            >
+              <option aria-label="None" value=""></option>
+              <option value="electronic">electronic</option>
+              <option value="furniture">furniture</option>
+              <option value="clothing">clothing</option>
+            </Select>
+
+
+        </FormControl>
+
+    </form>
     </>
   )
 }
