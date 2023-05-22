098518327dcd3187a06e484d370be52040b5f01b
gastonmatias <g.matiasvillagra@gmail.com>
2023-05-22 12:05:16 -0400

    GREEN - should display validations messages

--- pages/products/create.tsx
+++ pages/products/create.tsx
@@ -1,21 +1,69 @@
 import { NextPage } from "next"
-import { Typography, TextField, FormControl, InputLabel, Select, Button } from "@mui/material"
+import { Typography, TextField, FormControl, InputLabel, Select, Button, FormHelperText } from "@mui/material"
+import { FormEvent, HtmlHTMLAttributes, useState } from "react"
 
 const CreateProductPage: NextPage = () => {
+  
+  const [formErrors, setFormErrors] = useState({
+    name:'',
+    size:'',
+    type:'',
+  });
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault()
+
+    // const {name, size, type} = e.target.elements as HTMLFormControlsCollection
+    // const {name, size, type} = (e.target as HTMLFormElement).elements
+    
+    // const form = e.currentTarget as HTMLInputElement
+    // const form = e.target as HTMLInputElement
+    
+    // const {name, size, type} = form.elements!
+    
+    const {name, size, type} = e.target.elements
+    
+    // x la naturaleza asincrona del useState, es necesario utilizar el prevState,
+    // para qe la actualizacion del state sea correcta
+    if (!name.value) {
+      setFormErrors((prevState) => (
+          { ...prevState, name: "The name is required" }
+        )
+      )
+    }
+    
+    if (!size.value) {
+      setFormErrors((prevState) => (
+          { ...prevState, size: "The size is required" }
+        )
+      )
+    }
+    
+    if (!type.value) {
+      setFormErrors((prevState) => (
+          { ...prevState, type: "The type is required" }
+        )
+      )
+    }
+    
+  }
+  
   return (
     <>
     <Typography variant="h1" color="initial">Create Product</Typography>
     
-    <form>
+    <form onSubmit={handleSubmit}>
         <TextField
           id="name"
           label="name"
-        />
+          helperText={formErrors.name}
+          />
 
         <TextField
           id="size"
           label="size"
-        />
+          helperText={formErrors.size}
+          />
 
         <FormControl fullWidth>
             <InputLabel htmlFor="type" id="type">Type</InputLabel>
@@ -26,6 +74,7 @@ const CreateProductPage: NextPage = () => {
               labelId="type"
               id="type"
               label="Type"
+              // helperText={formErrors.size}
               inputProps={{
                 name: 'type',
                 id:'type',
@@ -37,8 +86,12 @@ const CreateProductPage: NextPage = () => {
               <option value="furniture">furniture</option>
               <option value="clothing">clothing</option>
             </Select>
+            
+            { formErrors.type && 
+              <FormHelperText>{formErrors.type}</FormHelperText>
+            }
 
-            <Button>Submit</Button>
+            <Button type="submit">Submit</Button>
         </FormControl>
 
     </form>
