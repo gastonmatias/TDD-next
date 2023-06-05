3c14d4223eab2b907a0dd7e12d5f46cdecd6b563
gastonmatias <g.matiasvillagra@gmail.com>
2023-06-05 16:06:52 -0400

    GREEN - when page is mounted: should be an input text filter by

--- pages/githubSearch.tsx
+++ pages/githubSearch.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next"
 import Typography from '@mui/material/Typography'
+import { TextField } from "@mui/material"
 
 const GithubSearchPage:NextPage = () => {
   return (
@@ -7,6 +8,8 @@ const GithubSearchPage:NextPage = () => {
     <Typography variant="h1" color="initial">
         Github repositories List
     </Typography>
+
+    <TextField id="filterBy" label="Filter by name" variant="outlined" />
     
 
     </>
