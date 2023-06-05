b4c2477ddc67560e048a435d0a5b1acb9d937217
gastonmatias <g.matiasvillagra@gmail.com>
2023-06-05 15:57:36 -0400

    GREEN - when page is mounted: should display the title

---
+++ pages/githubSearch.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next"
+import Typography from '@mui/material/Typography'
+
+const GithubSearchPage:NextPage = () => {
+  return (
+    <>
+    <Typography variant="h1" color="initial">
+        Github repositories List
+    </Typography>
+    
+
+    </>
+  )
+}
+export default GithubSearchPage
