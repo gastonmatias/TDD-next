00f9672444dd0fcd9eac3650c8fa3a836c4d4d39
gastonmatias <g.matiasvillagra@gmail.com>
2023-06-05 16:42:34 -0400

    REFACTORN - when page is mounted: should be an initial message _provide a search option & click the btn_

--- pages/githubSearch.tsx
+++ pages/githubSearch.tsx
@@ -1,36 +1,44 @@
 import { NextPage } from "next"
 import Typography from '@mui/material/Typography'
-import { TextField, Button, Container, Grid } from "@mui/material"
+import { TextField, Button, Container, Grid, Box } from "@mui/material"
 
 const GithubSearchPage:NextPage = () => {
   return (
     <>
     <Container>
-
-    <Typography variant="h2" color="white" sx={{my:3}}>
-        Github repositories List
-    </Typography>
-
-    <Grid container spacing={2} justifyContent='space-between'>
-
-        <Grid item md={6} xs={12}>
-            <TextField 
-                fullWidth id="filterBy" 
-                label="Filter by name" 
-                variant="outlined" 
-                />
-        </Grid>
-
-        <Grid item md={3} xs={12} sx={{display:'flex'}}>{/* flex para el stretch default */}
-            <Button fullWidth variant="contained" color="primary">
-              Search
-            </Button>
-        </Grid>
-
-        <Typography variant="h5" color="gray">
-            Please provide a search option and click in the search button
+        <Typography variant="h2" color="white" sx={{my:3}}>
+            Github repositories List
         </Typography>
-    </Grid>
+
+        <Grid container spacing={2} justifyContent='space-between'>
+
+            <Grid item md={6} xs={12}>
+                <TextField 
+                    fullWidth id="filterBy" 
+                    label="Filter by name" 
+                    variant="outlined" 
+                />
+            </Grid>
+
+            <Grid item md={3} xs={12} sx={{display:'flex'}}>{/* flex para el stretch default */}
+                <Button fullWidth variant="contained" color="primary">
+                  Search
+                </Button>
+            </Grid>
+        </Grid>
+
+        <Box
+            display='flex'
+            alignItems='center'
+            justifyContent='center'
+            height={400}
+        >
+            <Typography variant="h5" color="gray">
+                Please provide a search option and click in the search button
+            </Typography>
+        </Box>
+
+
 
     </Container>
     </>
